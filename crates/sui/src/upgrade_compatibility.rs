9131d1098adfa2694c4cf464c2f60d30013b7768
Jordan Jennings <jordan.jennings@mystenlabs.com>
2024-09-25 09:11:22 -0700

    add fields to enum instead of tuples

--- crates/sui/src/upgrade_compatibility.rs
+++ crates/sui/src/upgrade_compatibility.rs
@@ -3,58 +3,117 @@
 #![allow(unused_variables)]
 
 use std::collections::BTreeSet;
-use move_binary_format::compatibility::{Compatibility, CompatibilityMode};
+use move_binary_format::compatibility::{Compatibility};
+use move_binary_format::compatibility_mode::{CompatibilityMode};
 use move_binary_format::normalized::{Enum, Function, Struct};
 use move_core_types::account_address::AccountAddress;
 use move_core_types::identifier::{IdentStr, Identifier};
 use thiserror::Error;
 use anyhow::{anyhow};
 use move_core_types::language_storage::ModuleId;
+use sui_indexer::config::NameServiceOptions;
 
 /// Errors that can occur during upgrade compatibility checks.
 #[derive(Debug, Error)]
 enum UpgradeCompatibilityModeError {
-    #[error("Struct missing: {}", 0)]
+    #[error("Struct missing: {}", name.as_str())]
     StructMissing {
         name: Identifier,
         old_struct: Struct,
     },
-    #[error("Struct ability mismatch: {0:?} {1:?}")]
-    StructAbilityMismatch(Struct, Struct),
-    #[error("Struct type param mismatch: {0:?} {1:?}")]
-    StructTypeParamMismatch(Struct, Struct),
-    #[error("Struct field mismatch: {0:?} {1:?}")]
-    StructFieldMismatch(Struct, Struct),
-    #[error("Enum missing: {0:?}")]
-    EnumMissing(Enum),
-    #[error("Enum ability mismatch: {0:?} {1:?}")]
-    EnumAbilityMismatch(Enum, Enum),
-    #[error("Enum type param mismatch: {0:?} {1:?}")]
-    EnumTypeParamMismatch(Enum, Enum),
-    #[error("Enum new variant: {0:?} {1:?}")]
-    EnumNewVariant(Enum, Enum),
-    #[error("Enum variant missing: {0:?} {1:?}")]
-    EnumVariantMissing(Enum, usize),
-    #[error("Enum variant mismatch: {0:?} {1:?} {2:?}")]
-    EnumVariantMismatch(Enum, Enum, usize),
-    #[error("Function missing public: {0:?}")]
-    FunctionMissingPublic(Function),
-    #[error("Function missing friend: {0:?}")]
-    FunctionMissingFriend(Function),
-    #[error("Function missing entry: {0:?}")]
-    FunctionMissingEntry(Function),
-    #[error("Function signature mismatch: {}", name.as_str())]
+    #[error("Struct ability mismatch: {}\n  Old: {:?}\n  New: {:?}", name.as_str(), old_struct.abilities, new_struct.abilities)]
+    StructAbilityMismatch {
+        name: Identifier,
+        old_struct: Struct,
+        new_struct: Struct,
+    },
+    #[error("Struct type param mismatch: {}\n  Old: {:?}\n  New: {:?}", name.as_str(), old_struct.type_parameters, new_struct.type_parameters)]
+    StructTypeParamMismatch {
+        name: Identifier,
+        old_struct: Struct,
+        new_struct: Struct,
+    },
+    #[error("Struct field mismatch: {}\n  Old: {:?}\n  New: {:?}", name.as_str(), old_struct.fields, new_struct.fields)]
+    StructFieldMismatch {
+        name: Identifier,
+        old_struct: Struct,
+        new_struct: Struct,
+    },
+    #[error("Enum missing: {}\n  {:?}", name.as_str(), old_enum)]
+    EnumMissing {
+        name: Identifier,
+        old_enum: Enum,
+    },
+    #[error("Enum ability mismatch: {}\n  Old: {:?}\n  New: {:?}", name.as_str(), old_enum.abilities, new_enum.abilities)]
+    EnumAbilityMismatch {
+        name: Identifier,
+        old_enum: Enum,
+        new_enum: Enum,
+    },
+    #[error("Enum type param mismatch: {}\n  Old: {:?}\n  New: {:?}", name.as_str(), old_enum.type_parameters, new_enum.type_parameters)]
+    EnumTypeParamMismatch {
+        name: Identifier,
+        old_enum: Enum,
+        new_enum: Enum,
+    },
+    #[error("Enum new variant: {}\n  Old: {:?}\n  New: {:?}", name.as_str(), old_enum.variants, new_enum.variants)]
+    EnumNewVariant {
+        name: Identifier,
+        old_enum: Enum,
+        new_enum: Enum,
+    },
+    #[error("Enum variant missing: {}\n  {:?}", name.as_str(), old_enum.variants[*tag])]
+    EnumVariantMissing {
+        name: Identifier,
+        old_enum: Enum,
+        tag: usize,
+    },
+    #[error("Enum variant mismatch: {}\n  Old: {:?}\n  New: {:?}", name.as_str(), old_enum.variants[*tag], new_enum.variants[*tag])]
+    EnumVariantMismatch {
+        name: Identifier,
+        old_enum: Enum,
+        new_enum: Enum,
+        tag: usize,
+    },
+    #[error("Function missing public: {}", name.as_str())]
+    FunctionMissingPublic {
+        name: Identifier,
+        old_function: Function,
+    },
+    #[error("Function missing friend: {}", name.as_str())]
+    FunctionMissingFriend {
+        name: Identifier,
+        old_function: Function,
+    },
+    #[error("Function missing entry: {}", name.as_str())]
+    FunctionMissingEntry {
+        name: Identifier,
+        old_function: Function,
+    },
+    #[error("Function signature mismatch: {}\n  Old:\n    Params: {:?}\n    Return: {:?}\n  New:\n    Params: {:?}\n    Return: {:?}", name.as_str(),
+        old_function.parameters, old_function.return_, new_function.parameters, new_function.return_
+    )]
     FunctionSignatureMismatch {
         name: Identifier,
         old_function: Function,
         new_function: Function,
     },
-    #[error("Function lost public visibility: {0:?}")]
-    FunctionLostPublicVisibility(Function),
-    #[error("Function lost friend visibility: {0:?}")]
-    FunctionLostFriendVisibility(Function),
-    #[error("Function entry compatibility: {0:?} {1:?}")]
-    FunctionEntryCompatibility(Function, Function),
+    #[error("Function lost public visibility: {}", name.as_str())]
+    FunctionLostPublicVisibility {
+        name: Identifier,
+        old_function: Function,
+    },
+    #[error("Function lost friend visibility: {}", name.as_str())]
+    FunctionLostFriendVisibility {
+        name: Identifier,
+        old_function: Function,
+    },
+    #[error("Function entry compatibility: {}\n  Old: {:?}\n  New: {:?}", name.as_str(), old_function, new_function)]
+    FunctionEntryCompatibility {
+        name: Identifier,
+        old_function: Function,
+        new_function: Function,
+    },
     #[error("Friend module missing: {0:?} {1:?}")]
     FriendModuleMissing(BTreeSet<ModuleId>, BTreeSet<ModuleId>),
 }
@@ -78,6 +137,7 @@ impl CompatibilityMode for CliCompatibilityMode {
     fn module_id_mismatch(&mut self, _old_addr: &AccountAddress, _old_name: &IdentStr, _new_addr: &AccountAddress, _new_name: &IdentStr) {}
 
     fn struct_missing(&mut self, name: &Identifier, old_struct: &Struct) {
+        println!("WOT");
         self.errors.push(UpgradeCompatibilityModeError::StructMissing{
             name: name.clone(),
             old_struct: old_struct.clone()
@@ -85,51 +145,96 @@ impl CompatibilityMode for CliCompatibilityMode {
     }
 
     fn struct_ability_mismatch(&mut self, name: &Identifier, old_struct: &Struct, new_struct: &Struct) {
-        self.errors.push(UpgradeCompatibilityModeError::StructAbilityMismatch(old_struct.clone(), new_struct.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::StructAbilityMismatch{
+            name: name.clone(),
+            old_struct: old_struct.clone(),
+            new_struct: new_struct.clone()
+        });
     }
 
     fn struct_type_param_mismatch(&mut self, name: &Identifier, old_struct: &Struct, new_struct: &Struct) {
-        self.errors.push(UpgradeCompatibilityModeError::StructTypeParamMismatch(old_struct.clone(), new_struct.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::StructTypeParamMismatch {
+            name: name.clone(),
+            old_struct: old_struct.clone(),
+            new_struct: new_struct.clone()
+        });
     }
 
     fn struct_field_mismatch(&mut self, name: &Identifier, old_struct: &Struct, new_struct: &Struct) {
-        self.errors.push(UpgradeCompatibilityModeError::StructFieldMismatch(old_struct.clone(), new_struct.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::StructFieldMismatch {
+            name: name.clone(),
+            old_struct: old_struct.clone(),
+            new_struct: new_struct.clone()
+        });
     }
 
     fn enum_missing(&mut self, name: &Identifier, old_enum: &Enum) {
-        self.errors.push(UpgradeCompatibilityModeError::EnumMissing(old_enum.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::EnumMissing {
+            name: name.clone(),
+            old_enum: old_enum.clone()
+        });
     }
 
     fn enum_ability_mismatch(&mut self, name: &Identifier, old_enum: &Enum, new_enum: &Enum) {
-        self.errors.push(UpgradeCompatibilityModeError::EnumAbilityMismatch(old_enum.clone(), new_enum.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::EnumAbilityMismatch {
+            name: name.clone(),
+            old_enum: old_enum.clone(),
+            new_enum: new_enum.clone()
+        });
     }
 
     fn enum_type_param_mismatch(&mut self, name: &Identifier, old_enum: &Enum, new_enum: &Enum) {
-        self.errors.push(UpgradeCompatibilityModeError::EnumTypeParamMismatch(old_enum.clone(), new_enum.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::EnumTypeParamMismatch {
+            name: name.clone(),
+            old_enum: old_enum.clone(),
+            new_enum: new_enum.clone()
+        });
     }
 
     fn enum_new_variant(&mut self, name: &Identifier, old_enum: &Enum, new_enum: &Enum) {
-        self.errors.push(UpgradeCompatibilityModeError::EnumNewVariant(old_enum.clone(), new_enum.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::EnumNewVariant {
+            name: name.clone(),
+            old_enum: old_enum.clone(),
+            new_enum: new_enum.clone()
+        });
     }
 
-    fn enum_variant_missing(&mut self, name: &Identifier, old_enum: &Enum, variant_idx: usize) {
-        self.errors.push(UpgradeCompatibilityModeError::EnumVariantMissing(old_enum.clone(), variant_idx));
+    fn enum_variant_missing(&mut self, name: &Identifier, old_enum: &Enum, tag: usize) {
+        self.errors.push(UpgradeCompatibilityModeError::EnumVariantMissing {
+            name: name.clone(),
+            old_enum: old_enum.clone(),
+            tag,
+        });
     }
 
     fn enum_variant_mismatch(&mut self, name: &Identifier, old_enum: &Enum, new_enum: &Enum, variant_idx: usize) {
-        self.errors.push(UpgradeCompatibilityModeError::EnumVariantMismatch(old_enum.clone(), new_enum.clone(), variant_idx));
+        self.errors.push(UpgradeCompatibilityModeError::EnumVariantMismatch {
+            name: name.clone(),
+            old_enum: old_enum.clone(),
+            new_enum: new_enum.clone(),
+            tag: variant_idx
+        });
     }
 
     fn function_missing_public(&mut self, name: &Identifier, old_function: &Function) {
-        self.errors.push(UpgradeCompatibilityModeError::FunctionMissingPublic(old_function.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::FunctionMissingPublic {
+            name: name.clone(),
+            old_function: old_function.clone()
+        });
     }
 
     fn function_missing_friend(&mut self, name: &Identifier, old_function: &Function) {
-        self.errors.push(UpgradeCompatibilityModeError::FunctionMissingFriend(old_function.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::FunctionMissingFriend {
+            name: name.clone(),
+            old_function: old_function.clone()
+        });
     }
 
     fn function_missing_entry(&mut self, name: &Identifier, old_function: &Function) {
-        self.errors.push(UpgradeCompatibilityModeError::FunctionMissingEntry(old_function.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::FunctionMissingEntry {
+            name: name.clone(),
+            old_function: old_function.clone()
+        });
     }
 
     fn function_signature_mismatch(&mut self, name: &Identifier, old_function: &Function, new_function: &Function) {
@@ -141,15 +246,25 @@ impl CompatibilityMode for CliCompatibilityMode {
     }
 
     fn function_lost_public_visibility(&mut self, name: &Identifier, old_function: &Function) {
-        self.errors.push(UpgradeCompatibilityModeError::FunctionLostPublicVisibility(old_function.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::FunctionLostPublicVisibility {
+            name: name.clone(),
+            old_function: old_function.clone()
+        });
     }
 
     fn function_lost_friend_visibility(&mut self, name: &Identifier, old_function: &Function) {
-        self.errors.push(UpgradeCompatibilityModeError::FunctionLostFriendVisibility(old_function.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::FunctionLostFriendVisibility {
+            name: name.clone(),
+            old_function: old_function.clone()
+        });
     }
 
     fn function_entry_compatibility(&mut self, name: &Identifier, old_function: &Function, new_function: &Function) {
-        self.errors.push(UpgradeCompatibilityModeError::FunctionEntryCompatibility(old_function.clone(), new_function.clone()));
+        self.errors.push(UpgradeCompatibilityModeError::FunctionEntryCompatibility {
+            name: name.clone(),
+            old_function: old_function.clone(),
+            new_function: new_function.clone()
+        });
     }
 
     fn friend_module_missing(&mut self, old_modules: BTreeSet<ModuleId>, new_modules: BTreeSet<ModuleId>) {
