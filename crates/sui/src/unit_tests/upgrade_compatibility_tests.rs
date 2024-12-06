97acbe763c29dbab90a6b1ff163a369204ae4dd5
Jordan Jennings <jordan.jennings@mystenlabs.com>
2024-12-06 14:22:24 -0800

    format module missing error

--- crates/sui/src/unit_tests/upgrade_compatibility_tests.rs
+++ crates/sui/src/unit_tests/upgrade_compatibility_tests.rs
@@ -1,18 +1,20 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::upgrade_compatibility::compare_packages;
+use crate::upgrade_compatibility::{compare_packages, missing_module_diag};
 use insta::assert_snapshot;
 use move_binary_format::CompiledModule;
+use move_core_types::identifier::Identifier;
 use std::path::PathBuf;
+use std::str::FromStr;
 use sui_move_build::BuildConfig;
 use sui_move_build::CompiledPackage;
 use sui_types::move_package::UpgradePolicy;
 
 #[test]
 fn test_all() {
-    let (mods_v1, pkg_v2) = get_packages("all");
-    let result = compare_packages(mods_v1, pkg_v2, UpgradePolicy::Compatible);
+    let (mods_v1, pkg_v2, path) = get_packages("all");
+    let result = compare_packages(mods_v1, pkg_v2, path, UpgradePolicy::Compatible);
 
     assert!(result.is_err());
     let err = result.unwrap_err();
@@ -21,8 +23,8 @@ fn test_all() {
 
 #[test]
 fn test_declarations_missing() {
-    let (pkg_v1, pkg_v2) = get_packages("declaration_errors");
-    let result = compare_packages(pkg_v1, pkg_v2, UpgradePolicy::Compatible);
+    let (pkg_v1, pkg_v2, path) = get_packages("declaration_errors");
+    let result = compare_packages(pkg_v1, pkg_v2, path, UpgradePolicy::Compatible);
 
     assert!(result.is_err());
     let err = result.unwrap_err();
@@ -31,8 +33,8 @@ fn test_declarations_missing() {
 
 #[test]
 fn test_function() {
-    let (pkg_v1, pkg_v2) = get_packages("function_errors");
-    let result = compare_packages(pkg_v1, pkg_v2, UpgradePolicy::Compatible);
+    let (pkg_v1, pkg_v2, path) = get_packages("function_errors");
+    let result = compare_packages(pkg_v1, pkg_v2, path, UpgradePolicy::Compatible);
 
     assert!(result.is_err());
     let err = result.unwrap_err();
@@ -41,8 +43,8 @@ fn test_function() {
 
 #[test]
 fn test_struct() {
-    let (pkg_v1, pkg_v2) = get_packages("struct_errors");
-    let result = compare_packages(pkg_v1, pkg_v2, UpgradePolicy::Compatible);
+    let (pkg_v1, pkg_v2, path) = get_packages("struct_errors");
+    let result = compare_packages(pkg_v1, pkg_v2, path, UpgradePolicy::Compatible);
 
     assert!(result.is_err());
     let err = result.unwrap_err();
@@ -51,8 +53,8 @@ fn test_struct() {
 
 #[test]
 fn test_enum() {
-    let (pkg_v1, pkg_v2) = get_packages("enum_errors");
-    let result = compare_packages(pkg_v1, pkg_v2, UpgradePolicy::Compatible);
+    let (pkg_v1, pkg_v2, path) = get_packages("enum_errors");
+    let result = compare_packages(pkg_v1, pkg_v2, path, UpgradePolicy::Compatible);
 
     assert!(result.is_err());
     let err = result.unwrap_err();
@@ -61,8 +63,8 @@ fn test_enum() {
 
 #[test]
 fn test_type_param() {
-    let (pkg_v1, pkg_v2) = get_packages("type_param_errors");
-    let result = compare_packages(pkg_v1, pkg_v2, UpgradePolicy::Compatible);
+    let (pkg_v1, pkg_v2, path) = get_packages("type_param_errors");
+    let result = compare_packages(pkg_v1, pkg_v2, path, UpgradePolicy::Compatible);
 
     assert!(result.is_err());
     let err = result.unwrap_err();
@@ -71,19 +73,34 @@ fn test_type_param() {
 
 #[test]
 fn test_friend_link_ok() {
-    let (pkg_v1, pkg_v2) = get_packages("friend_linking");
+    let (pkg_v1, pkg_v2, path) = get_packages("friend_linking");
     // upgrade compatibility ignores friend linking
-    assert!(compare_packages(pkg_v1, pkg_v2, UpgradePolicy::Compatible).is_ok());
+    assert!(compare_packages(pkg_v1, pkg_v2, path, UpgradePolicy::Compatible).is_ok());
 }
 
 #[test]
 fn test_entry_linking_ok() {
-    let (pkg_v1, pkg_v2) = get_packages("entry_linking");
+    let (pkg_v1, pkg_v2, path) = get_packages("entry_linking");
     // upgrade compatibility ignores entry linking
-    assert!(compare_packages(pkg_v1, pkg_v2, UpgradePolicy::Compatible).is_ok());
+    assert!(compare_packages(pkg_v1, pkg_v2, path, UpgradePolicy::Compatible).is_ok());
 }
 
-fn get_packages(name: &str) -> (Vec<CompiledModule>, CompiledPackage) {
+#[test]
+fn test_malformed_toml() {
+    for malformed_pkg in &["empty", "whitespace"] {
+        let move_pkg_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("src/unit_tests/fixtures/upgrade_errors/malformed_move_toml/")
+            .join(malformed_pkg);
+        assert!(
+            missing_module_diag(&Identifier::from_str("identifier").unwrap(), &move_pkg_path)
+                .is_ok(),
+            "Unable to give diag for: {}",
+            malformed_pkg
+        );
+    }
+}
+
+fn get_packages(name: &str) -> (Vec<CompiledModule>, CompiledPackage, PathBuf) {
     let mut path: PathBuf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     path.push("src/unit_tests/fixtures/upgrade_errors/");
     path.push(format!("{}_v1", name));
@@ -99,7 +116,7 @@ fn get_packages(name: &str) -> (Vec<CompiledModule>, CompiledPackage) {
 
     let pkg_v2 = BuildConfig::new_for_testing().build(&path).unwrap();
 
-    (mods_v1, pkg_v2)
+    (mods_v1, pkg_v2, path)
 }
 
 /// Snapshots will differ on each machine, normalize to prevent test failures
